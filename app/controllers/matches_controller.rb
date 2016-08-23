7d82bba2ae76f33ad6595a0a9e1470735869bc3c
Pawan Agarawal <pawan@rigup.com>
2016-08-23 01:16:21 -0500

    Fix match index queries

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -4,20 +4,25 @@ class MatchesController < RestfulController
     get_my_matches = params[:my_matches]
     get_past_matches = params[:past_matches]
 
-    scope = scope.group('matches.id')
-                .select('array_agg(match_users.user_id) AS user_ids')
-                .select('count(match_users) AS num_users')
+    scope = scope.joins(:match_users)
+                .select('matches.*')
+                .group('matches.id')
 
+    #TODO: These queries may need to be re-examined for performance
     if get_requests == 'true'
-      scope = scope.where('num_users > (CASE WHEN matches.is_singles THEN 2 ELSE 4 END ')
-                  .where('? NOT IN (user_ids)', User.current_user.id)
-                  .where('matches.match_date >= now()')
+      scope = scope.where('(SELECT count(users.id) FROM users WHERE users.id = match_users.user_id) < ' +
+                            '(CASE WHEN matches.is_singles THEN 2 ELSE 4 END)')
+                  .where('? != ANY (SELECT id FROM users WHERE users.id = match_users.user_id)',
+                         User.current_user.id)
+                  .where('matches.match_date >= CURRENT_DATE')
     elsif get_my_matches == 'true'
-      scope = scope.where('? IN (user_ids)', User.current_user.id)
-                  .where('matches.match_date >= now()')
+      scope = scope.where('? = ANY (SELECT id FROM users WHERE users.id = match_users.user_id)',
+                          User.current_user.id)
+                  .where('matches.match_date >= CURRENT_DATE')
     elsif get_past_matches == 'true'
-      scope = scope.where('? IN (user_ids)', User.current_user.id)
-                  .where('matches.match_date < now()')
+      scope = scope.where('? = ANY (SELECT id FROM users WHERE users.id = match_users.user_id)',
+                          User.current_user.id)
+                  .where('matches.match_date < CURRENT_DATE')
     end
 
     return scope
