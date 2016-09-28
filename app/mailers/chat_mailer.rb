e1e7485055d931ee0d1ff9d65248a620e6a9bd46
Pawan Agarawal <pawan@rigup.com>
2016-09-28 01:23:05 -0500

    Update new message email

--- app/mailers/chat_mailer.rb
+++ app/mailers/chat_mailer.rb
@@ -1,22 +1,20 @@
 class ChatMailer < ApplicationMailer
   def self.new_messages(message)
-    emails = message.chat.users.pluck(:email)
-    emails.delete(message.user.email)
-
-    emails.each { |email|
-      ChatMailer.new_message(message, email).deliver_later
+    message.chat.users.each { |user|
+      ChatMailer.new_message(message, user).deliver_later if user.id != message.user.id
     }
   end
 
   def new_message(message, recipient)
     chat = message.chat
     match = chat.match
-    @user_name = message.user.first_name
+    @sender_name = message.user.first_name
+    @recipient_name = recipient.first_name
     @match_time = match.match_date.strftime('%-l:%M %p %a, %-m/%-d')
-    @match_court = match.court.name
+    @match_type = match.is_singles ? 'Singles' : 'Doubles'
     @text = message.body
     @url_ext = chat.id.to_s + '/' + match.id.to_s
 
-    mail(to: recipient, subject: "New message from #{@user_name} about your match")
+    mail(to: recipient.email, subject: "New message from #{@user_name} about your match")
   end
 end
