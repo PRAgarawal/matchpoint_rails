e8a129373a9a1f17bc5f8fbb75461cf3f8d31205
Raj Agarawal <raj@matchpoint.us>
2016-11-22 19:13:22 +0000

    Allow empty addresses for courts.

--- app/models/postal_address.rb
+++ app/models/postal_address.rb
@@ -2,10 +2,10 @@ class PostalAddress < ApplicationRecord
   # The thing this address is attached to.
   belongs_to :postal_addressable, polymorphic: true
 
-  validates :street, presence: true, length: {maximum: 200}
-  validates :state, presence: true, length: {is: 2}
-  validates :city, presence: true, length: {maximum: 100}
-  validates :zip, presence: true, length: {is: 5}
+  #validates :street, presence: true, length: {maximum: 200}
+  #validates :state, presence: true, length: {is: 2}
+  #validates :city, presence: true, length: {maximum: 100}
+  #validates :zip, presence: true, length: {is: 5}
 
   def line_1
     return self.street
