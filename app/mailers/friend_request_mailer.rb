a1b4f810d56fe7c9b8033072ada9a037f9d9bb3f
Pawan Agarawal <pawan@rigup.com>
2016-11-24 04:19:02 -0600

    Fix #83, use site colors in emails

--- app/mailers/friend_request_mailer.rb
+++ app/mailers/friend_request_mailer.rb
@@ -5,6 +5,18 @@ class FriendRequestMailer < ApplicationMailer
     @friend_name = "#{friend.first_name} #{friend.last_name}"
     @friend_first_name = friend.first_name
 
-    mail(to: user.email, subject: "#{@friend_name} wants to be your friend!")
+    mail(to: user.email, subject: "#{@friend_name} wants to be your friend on Match Point")
+  end
+
+  def request_accepted(user, friend)
+    @user_name = user.first_name
+    @friend_name = "#{friend.first_name} #{friend.last_name}"
+    @friend_first_name = friend.first_name
+    @is_from_inviter = friend.invited_by_id == user.id
+    subject = @is_from_inviter ?
+        "#{@friend_name} has accepted your invitation to join Match Point" :
+        "#{@friend_name} has accepted your friend request on Match Point"
+
+    mail(to: user.email, subject: subject)
   end
 end
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -9,6 +9,8 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User', foreign_key: 'friend_id'
 
+  before_save :notify_friend_requester, if: :is_confirmed_changed?
+
   def not_adding_self
     errors.add(:friend_id, 'You cannot add yourself as a friend.') if user_id == friend_id
   end
@@ -17,5 +19,9 @@ class Friendship < ApplicationRecord
     self.where("(user_id = #{user_id} AND friend_id = #{friend_id}) OR " +
                "(user_id = #{friend_id} AND friend_id = #{user_id})")
         .first
+  end
+
+  def notify_friend_requester
+    FriendRequestMailer.request_accepted(user, friend).deliver_later if is_confirmed
   end
 end
