7b6bbe42a68b4b5145ac97266444923774c9cce1
Sri Paladugu <sri@matchpoint.us>
2017-02-05 22:44:44 -0800

    Add metro property to mixpanel people profile

--- app/assets/matchpoint/main/courts/courts-module.js
+++ app/assets/matchpoint/main/courts/courts-module.js
@@ -83,6 +83,7 @@ courtsModule.controller('JoinCourtModalController',
         resources.all('courts/join/' + court.id).customPOST().then(function () {
           mixPanelEvts.joinCourt(court);
           $scope.user.has_joined_courts = true;
+          mixpanel.people.set({"Metro" : ($scope.user.is_dfw ? "DFW" : "LA")});
           $modalInstance.close('success');
         });
       };
