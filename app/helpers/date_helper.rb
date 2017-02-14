0bf8a21bf5bf42b8ceed1680d17f30947f607537
Raj Agarawal <raj@matchpoint.us>
2017-02-14 15:17:01 -0600

    Remove unnecessary class

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -1,11 +1,9 @@
 module DateHelper
-  class DateHelper
-    def self.timezone
-      return User.current_user.is_dfw ? 'America/Chicago' : 'America/Los_Angeles'
-    end
+  def self.timezone
+    return User.current_user.is_dfw ? 'America/Chicago' : 'America/Los_Angeles'
+  end
 
-    def self.today_cutoff
-      return Time.now.in_time_zone(self.timezone).beginning_of_day
-    end
+  def self.today_cutoff
+    return Time.now.in_time_zone(self.timezone).beginning_of_day
   end
 end
